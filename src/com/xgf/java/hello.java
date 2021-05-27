06dc5d26f05f916e2473ce67c5bba8de0100e2d3
biggf <3210678098@qq.com>
2021-05-27 20:41:45 +0800

    "hello"

--- src/com/xgf/java/hello.java
+++ src/com/xgf/java/hello.java
@@ -8,6 +8,9 @@ public class hello {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println("i = " + i);
+            for (String arg : args) {
+                
+            }
         }
     }
     
