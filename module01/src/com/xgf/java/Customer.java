534e48815fdeb6ea5e44e00a07bbe2c786063fd1
biggf <3210678098@qq.com>
2021-05-27 20:01:36 +0800

    Initial commit

---
+++ module01/src/com/xgf/java/Customer.java
@@ -0,0 +1,86 @@
+package com.xgf.java;
+
+import java.util.ArrayList;
+
+//单行注释
+public class Customer {
+    public void testCustrome(){
+
+    }
+    //prsf
+    private static final void getxx(int x){
+
+    }
+    //psf
+    public static final void setxx(int x){
+
+    }
+    public static final String s = "";
+    public static final String String (int x){
+        return "";
+    }
+    //模板一 psvm
+    public static void main(String[] args){
+        System.out.println("hello world!!!");
+
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+    }
+
+    public int getNum(int x, int y){
+        //sout
+        System.out.println("hello");
+        //soutp
+        System.out.println("x = " + x + ", y = " + y);
+        //soutm
+        System.out.println("Customer.getNum");
+        //soutv
+        System.out.println("x = " + x);
+        //fori
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        //iter
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Integer integer : list) {
+
+        }
+
+
+
+        //list.for
+
+
+        for (Integer integer : list) {
+
+        }
+        //list.fori
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        //list.forr
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+        }
+        //ifn
+        if (list == null) {
+            
+        }
+        //inn
+        if (list != null) {
+
+        }
+
+        // xx.nn/xxx.null
+        if (list != null) {
+
+        }
+        if (list == null) {
+
+        }
+        //psvm
+
+        return 0;
+    }
+}
---
+++ src/com/xgf/java/hello.java
@@ -0,0 +1,8 @@
+package com.xgf.java;
+
+/**
+ * @author biggf
+ * @date 2021/5/27 0027-12:37
+ */
+public class hello {
+}
